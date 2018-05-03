7c38e936207cb11749beaacca56fcc28462f28dd
nikolay <nikolay.grozev@gmail.com>
2018-05-03 21:29:07 +1000

    Convrsion from vtt to srt.

--- subtitles.sh
+++ subtitles.sh
@@ -12,6 +12,15 @@ set -u
 ### How to set it up ... 
 chmod u+x ./OpenSubtitlesDownload/OpenSubtitlesDownload.py 
 
+# Convert VTT to SRT
+find "$1" -type f | egrep -i "vtt$" | egrep -v '\/original' | while read -r file
+do
+    echo ${file}
+    fileNoExt=`echo "${file}" | sed -e 's/\.vtt$//g' | sed -e 's/\.en$//g'`
+    ffmpeg -nostats -loglevel panic -y -i "${file}" "${fileNoExt}.srt" && rm "${file}"
+done
+
+# Download From Open Subtitles
 find "$1" -type f | egrep -i "m4v|mkv|mp4|avi|mov|wmv|flv" | egrep -v '\/original' | while read -r file
 do
     fileNoExt=`echo "${file}" | sed -e 's/\.[^\.]*$//g'`
